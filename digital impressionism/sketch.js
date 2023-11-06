0a34ce29ec7294196b1220d10e5a8617d217e826
archiee-exe <kai-aditya@hotmail.com>
2023-11-06 11:50:10 -0800

    Add files via upload

--- digital impressionism/sketch.js
+++ digital impressionism/sketch.js
@@ -9,14 +9,15 @@ function setup() {
 //nested loop is loop inside another loop.
 
 function draw() {
-  let rand = random(20,100);
-  let colorgrades = ['255', '125', '5', '90']
-  for (let i = 0; i < 1200; i += 25) {
-    for (let j = 0; j < 1200; j += 25) {
+  let rand = random(20,mouseY/4);
+    let rand2=random(0,mouseX);
+  let colorgrades = ['255', '125', '5', '60', '90']
+  for (let i = 0; i < 1200; i += 30) {
+    for (let j = 0; j < 1200; j += 30) {
       let rand1 = random (colorgrades);
-      fill (rand,rand1,rand1,mouseX/10);
-       circle (i+5,j,random(20,40));
-       fill (150,13,random(140,150),100);
+      fill (rand,rand1,rand2,mouseX/10);
+       circle (i+5,j,random(20,45));
+       fill (200,rand,random(140,150),80);
        circle (mouseY,mouseX,50);
       console.log("rand1 being used " + rand1);
       console.log("rand" + rand);
